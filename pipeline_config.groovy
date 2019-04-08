16a0a694ce0aab349b0f1d36dc9b5ef930aba3d6
steven-terrana <steventerrana@gmail.com>
2019-04-08 13:34:34 -0400

    Create pipeline_config.groovy

---
+++ pipeline_config.groovy
@@ -0,0 +1,11 @@
+/*
+  This file will inherit the configurations set via: 
+    https://github.com/steven-terrana/example-jte-configuration/blob/master/pipeline_config.groovy
+  
+  This repository level configuration file should now add configs 
+  that are specific to this repo, such as the use of gradle for build() and
+  unit_test() 
+*/
+libraries{
+  gradle
+}
